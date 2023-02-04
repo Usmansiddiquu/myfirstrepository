1e08d2faa61d387ca7b850b3380017e83a8e3a00
Usmansiddiquu <121655186+Usmansiddiquu@users.noreply.github.com>
2023-02-04 12:39:36 +0500

    js2ndassignment

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
+// var username = ("Muhammad Usman")
+// var myName = ("Siddiqui")
+// alert(username+ " " +myName)
+// var titledmessage =("Hello World")
+// alert(titledmessage)
+// var age =("im 18 years old")
+// alert(age)
+// var scrip = ("PIZZA\nPIZZ\nPIZ\nPI\nP")
+// alert(scrip)
+// var email =("usmansiddiquimuhammad@gmail.com")
+// alert(email)
+// var book = (" “A smarter way to learn JavaScript”.")
+// alert(book)
+// document.write("Yah! I can write HTML content through JavaScript")
+// var string = (" “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// alert(string)
+    
+// chapter # 03
+
+// var visits = (" “You have visited this site 14 times”.")
+//  alert(visits)
+// document.write (" “usman ordered 5 T-shirt(s) on  daraz”.")
+// var firstname = ("Muhammad ") 
+// var middlename = ("usman ") 
+// var lastname = ("siddiqui") 
+// alert(firstname+middlename+lastname)
+ 
+// legal variables
+
+// var firstname = ("Muhammad ") 
+// var middlename = ("usman ") 
+// var lastname = ("siddiqui")
+// var email =("usmansiddiquimuhammad@gmail.com")
+// var book = (" “A smarter way to learn JavaScript”.")
+
+// illegal variables
+
+// var var =("aaabv")
+// var alert =("xdgd")
+// var var =("aaabv")
+// var alert =("xdgd")
+// var var =("aaabv")
+
+document.write("<h1> Rules for naming JS variables </h1>")
+document.write ("<h6>Variable names can only contain,numbers,$and. For example:my_1stvariable</h6>")
+document.write ("<h6>Variables must begin with a letter.$ or_.For example $name, _name or name</h6>")
+document.write ("<h6>Variable names are case sensitive</h6>")
+document.write ("<h6> Variable names should not be JS keywords</h6>")
 
